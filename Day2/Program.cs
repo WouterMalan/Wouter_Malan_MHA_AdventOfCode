31cdff98c6911d42be646a92c6bccd78c6593e66
Wouter Malan <WouterM@gobid.co.za>
2025-04-16 14:33:41 +0200

    Add commented-out implementation for CountSafeReports and IsSafeReport methods

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -11,6 +11,60 @@ class Program
         Console.WriteLine($"Number of safe reports: {safeReports}");
     }
     
+    //Part 1
+    // static int CountSafeReports(string[] reports)
+    // {
+    //     if (reports == null || reports.Length == 0)
+    //     {
+    //         return 0;
+    //     }
+    //
+    //     int safeCount = 0;
+    //
+    //     foreach (var report in reports)
+    //     {
+    //         var levels = report
+    //             .Split(' ')
+    //             .Select(int.Parse)
+    //             .ToList();
+    //
+    //         if (IsSafeReport(levels))
+    //         {
+    //             safeCount++;
+    //         }
+    //     }
+    //     
+    //     return safeCount;
+    // }
+    //
+    // private static bool IsSafeReport(List<int> levels)
+    // {
+    //     if (levels.Count < 2)
+    //     {
+    //         return false;
+    //     }
+    //         
+    //     bool isIncreasing = levels[1] > levels[0];
+    //
+    //     for (int i = 1; i < levels.Count; i++)
+    //     {
+    //         int difference = levels[i] - levels[i - 1];
+    //             
+    //         if(Math.Abs(difference) < 1 || Math.Abs(difference) > 3)
+    //         {
+    //             return false;
+    //         }
+    //             
+    //         if((isIncreasing && difference < 0) || (!isIncreasing && difference > 0))
+    //         {
+    //             return false;
+    //         }
+    //     }
+    //     
+    //     return true;
+    // }
+    
+    //Part 2
     static int CountSafeReports(string[] reports)
     {
         if (reports == null || reports.Length == 0)
