e682c8e155ed08dfac7b37a50e6f342a7f0eeaa3
Wouter <woutermalan7909@gmail.com>
2025-04-18 07:34:14 +0200

    Cleaned up day 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+﻿//Input file is in bin folder
 
 namespace Day2;
 
@@ -7,65 +7,71 @@ class Program
     static void Main()
     {
         string[] input = System.IO.File.ReadAllLines("Input.txt");
-        int safeReports = CountSafeReports(input);
-        Console.WriteLine($"Number of safe reports: {safeReports}");
+        
+        //Part 1
+        int safeReports1 = CountSafeReportsPart1(input);
+        Console.WriteLine($"PART 1: Number of safe reports: {safeReports1}");;
+        
+        //Part 2
+        int safeReports2 = CountSafeReportsPart2(input);
+        Console.WriteLine($"PART 1: Number of safe reports: {safeReports2}");;
     }
     
     //Part 1
-    // static int CountSafeReports(string[] reports)
-    // {
-    //     if (reports == null || reports.Length == 0)
-    //     {
-    //         return 0;
-    //     }
-    //
-    //     int safeCount = 0;
-    //
-    //     foreach (var report in reports)
-    //     {
-    //         var levels = report
-    //             .Split(' ')
-    //             .Select(int.Parse)
-    //             .ToList();
-    //
-    //         if (IsSafeReport(levels))
-    //         {
-    //             safeCount++;
-    //         }
-    //     }
-    //     
-    //     return safeCount;
-    // }
-    //
-    // private static bool IsSafeReport(List<int> levels)
-    // {
-    //     if (levels.Count < 2)
-    //     {
-    //         return false;
-    //     }
-    //         
-    //     bool isIncreasing = levels[1] > levels[0];
-    //
-    //     for (int i = 1; i < levels.Count; i++)
-    //     {
-    //         int difference = levels[i] - levels[i - 1];
-    //             
-    //         if(Math.Abs(difference) < 1 || Math.Abs(difference) > 3)
-    //         {
-    //             return false;
-    //         }
-    //             
-    //         if((isIncreasing && difference < 0) || (!isIncreasing && difference > 0))
-    //         {
-    //             return false;
-    //         }
-    //     }
-    //     
-    //     return true;
-    // }
+    static int CountSafeReportsPart1(string[] reports)
+    {
+        if (reports == null || reports.Length == 0)
+        {
+            return 0;
+        }
+    
+        int safeCount = 0;
+    
+        foreach (var report in reports)
+        {
+            var levels = report
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+    
+            if (IsSafeReportPart1(levels))
+            {
+                safeCount++;
+            }
+        }
+        
+        return safeCount;
+    }
+    
+    private static bool IsSafeReportPart1(List<int> levels)
+    {
+        if (levels.Count < 2)
+        {
+            return false;
+        }
+            
+        bool isIncreasing = levels[1] > levels[0];
+    
+        for (int i = 1; i < levels.Count; i++)
+        {
+            int difference = levels[i] - levels[i - 1];
+                
+            if(Math.Abs(difference) < 1 || Math.Abs(difference) > 3)
+            {
+                return false;
+            }
+                
+            if((isIncreasing && difference < 0) || (!isIncreasing && difference > 0))
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
     
     //Part 2
-    static int CountSafeReports(string[] reports)
+    static int CountSafeReportsPart2(string[] reports)
     {
         if (reports == null || reports.Length == 0)
         {
@@ -81,7 +87,7 @@ class Program
                 .Select(int.Parse)
                 .ToList();
 
-            if (IsSafeReport(levels))
+            if (IsSafeReportPart2(levels))
             {
                 safeCount++;
             }
@@ -90,7 +96,7 @@ class Program
         return safeCount;
     }
 
-    private static bool IsSafeReport(List<int> levels)
+    private static bool IsSafeReportPart2(List<int> levels)
     {
         if (levels.Count < 2)
         {
